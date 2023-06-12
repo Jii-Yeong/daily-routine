cf2df20369e2397738362d5227123205ef81052e
Jii-Yeong <tj5134@naver.com>
2023-06-12 16:51:49 +0900

    fix: 로그인 상태가 아닐 때 처리 추가

--- src/hooks/todo/useTodoList.ts
+++ src/hooks/todo/useTodoList.ts
@@ -6,41 +6,48 @@ import {useRecoilValue} from "recoil";
 import {userProfileSelector} from "@/recoil/user/user-selectors.ts";
 
 export const useTodoList = () => {
-
   const [todoList, setTodoList] = useState<TodoItemModel[]>([])
   const userData = useRecoilValue(userProfileSelector)
+  const userId = userData?.id
 
-  useEffect(() => {
-    const fetchTodoList = async () => {
-      const userId = userData?.id
-      if (!userId) return
+  const fetchTodoList = async () => {
+    if (!userId) return
 
-      const data = await getTodoList(userId)
-      if (!data) return
+    const data = await getTodoList(userId)
+    if (!data) return
 
-      const parsedData = data.map(item => toTodoItemModel(item))
-      setTodoList(parsedData)
-    }
-    fetchTodoList()
-  }, [])
+    const parsedData = data.map(item => toTodoItemModel(item))
+    setTodoList(parsedData)
+  }
+
 
   const enterTodoItem = async (text: string) => {
+    if (!userId) {
+      setTodoList([
+        ...todoList,
+        {
+          id: Math.random(),
+          text,
+          checked: false,
+        }])
+      return
+    }
     await addTodoItem(text)
-    setTodoList([
-      ...todoList,
-      {
-        id: Math.random(),
-        text,
-        checked: false,
-      }])
+    await fetchTodoList()
   }
 
   const clickCheckboxButton = async (id: number, checked: boolean) => {
+    if (!userId) return
+
     const todoItem = {
       checked
     }
     await updateTodoItem(id, todoItem)
   }
+
+  useEffect(() => {
+    fetchTodoList()
+  }, [])
 
   return {
     todoList,

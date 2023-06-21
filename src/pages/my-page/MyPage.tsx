0af79c78fb6317e501e183612a7cad4f742f9b5f
Jii-Yeong <tj5134@naver.com>
2023-06-21 18:11:17 +0900

    fix: 마이페이지도 동일하게 로그아웃 시 홈으로 이동

--- src/pages/my-page/MyPage.tsx
+++ src/pages/my-page/MyPage.tsx
@@ -3,8 +3,22 @@ import CategoryTodoListChart from "@/components/my-page/CategoryTodoListChart/Ca
 import CheckTodoListChart from "@/components/my-page/CheckTodoListChart/CheckTodoListChart"
 import DateTodoListChart from "@/components/my-page/DateTodoListChart/DateTodoListChart"
 import MyPageSidebar from "@/components/sidebar/MyPageSidebar/MyPageSidebar"
+import supabaseAdmin from "@/supabase/init"
+import { getRootPage } from "@/utils/page.utils"
+import { useCallback, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function MyPage() {
+  const navigate = useNavigate()
+  const goRootPageToNotUser = useCallback(async () => {
+    const { data } = await supabaseAdmin.auth.getSession()
+    if (!data.session?.user) navigate(getRootPage())
+  }, [navigate])
+
+  useEffect(() => {
+    goRootPageToNotUser()
+  }, [goRootPageToNotUser])
+
   return (
     <DefaultLayout sidebarChildren={<MyPageSidebar />}>
       <div className="main-container">
